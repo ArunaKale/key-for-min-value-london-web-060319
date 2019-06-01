c3aa0b273696223db8c561461761a011b0a28d5d
AN K <a2nkale@gmail.com>
2019-06-01 20:35:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -31,22 +31,7 @@ def key_for_min_value(name_hash)
 end
 
 
-def key_for_min_value(name_hash)
-  # code goes here
-  min_value = nil
-  min_key = nil
-  name_hash.each do |k, v|
-    if min_value == nil
-      min_value = v
-      min_key = k
-    elsif min_value > v
-      min_value = v
-      min_key = k
-    end
-  end
-  min_key
-end
-end 
+
 
 def key 
 end 
